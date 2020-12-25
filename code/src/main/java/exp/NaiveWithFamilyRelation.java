5f95cbd281d80d5fa4b7a8daeb0cb045b5383bb7
Ruoyu Wang <wang.ruoyu@sjtu.edu.cn>
2020-12-25 20:06:36 +0800

    模拟一个家庭数据库（带噪音）做一个理想实验，记录结果

--- code/src/main/java/exp/NaiveWithFamilyRelation.java
+++ code/src/main/java/exp/NaiveWithFamilyRelation.java
@@ -7,16 +7,27 @@ import java.io.File;
 import java.io.IOException;
 
 public class NaiveWithFamilyRelation {
-    private static void genData(double errorRate, int familyCnt, String dataPath) throws IOException  {
-        System.out.printf("\n>>> Generating Data(Error: %f; Families: %d; Path)...\n", errorRate, familyCnt);
+    private static void genData(double errorRate, int familyCnt, String dataPath, String suffix) throws IOException  {
+        System.out.printf(
+                "\n>>> Generating Data[%s](Error: %f; Families: %d; Path: %s)...\n",
+                suffix, errorRate, familyCnt, dataPath
+        );
         FamilyRelationGenerator.ERROR_PROB = errorRate;
         FamilyRelationGenerator.FAMILY_CNT = familyCnt;
         File data_dir = new File(dataPath);
         if (!data_dir.exists()) {
             data_dir.mkdirs();
         }
-        FamilyRelationGenerator.generateSimple(dataPath);
-        FamilyRelationGenerator.generateMedium(dataPath);
+        switch (suffix) {
+            case "Simple":
+                FamilyRelationGenerator.generateSimple(dataPath);
+                break;
+            case "Medium":
+                FamilyRelationGenerator.generateMedium(dataPath);
+                break;
+            default:
+                throw new IOException("Wrong suffix: " + suffix);
+        }
     }
 
     private static void runAlg(String bkPath, String hypothesisPath) {
@@ -30,15 +41,11 @@ public class NaiveWithFamilyRelation {
         double error_rate = Double.parseDouble(args[0]);
         int family_cnt = Integer.parseInt(args[1]);
         String data_path = args[2];
-        genData(error_rate, family_cnt, data_path);
+        String suffix = args[3];
+        genData(error_rate, family_cnt, data_path, suffix);
         runAlg(
-                String.format("%s/FamilyRelationSimple(%f)(%dx).tsv", data_path, error_rate, family_cnt),
-                "HypothesisHumanSimple.amie"
-        );
-        System.out.println("-----");
-        runAlg(
-                String.format("%s/FamilyRelationMedium(%f)(%dx).tsv", data_path, error_rate, family_cnt),
-                "HypothesisHumanMedium.amie"
+                String.format("%s/FamilyRelation%s(%f)(%dx).tsv", data_path, suffix, error_rate, family_cnt),
+                String.format("HypothesisHuman%s.amie", suffix)
         );
     }
 }
--- experiments/NaiveWithFamilyRelation/run.sh
+++ experiments/NaiveWithFamilyRelation/run.sh
@@ -7,11 +7,12 @@ export LD_PRELOAD=/home/trams/lib/swipl/lib/x86_64-linux/libswipl.so
 for DUPLICATE in $(seq 1 1 1)
 do
     echo DUPLICATE: $DUPLICATE
-    for ERROR_RATE in $(seq 0 0.05 0.3)
+    for ERROR_RATE in $(seq 0 0.05 0.5)
     do
-        for ((FAMILY_CNT=1; FAMILY_CNT<=100; FAMILY_CNT*=10 ))
+        for ((FAMILY_CNT=1; FAMILY_CNT<=1000; FAMILY_CNT*=10 ))
         do
-            java -jar ExpNaive.jar $ERROR_RATE $FAMILY_CNT DUP_$DUPLICATE
+            # java -jar ExpNaive.jar $ERROR_RATE $FAMILY_CNT DUP_$DUPLICATE Simple
+            java -jar ExpNaive.jar $ERROR_RATE $FAMILY_CNT DUP_$DUPLICATE Medium
         done
     done
 done
